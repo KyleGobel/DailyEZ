ad5c948b5eb4193f732c38e7e53c3932363359ba
Kyle Gobel <kgobel@gmail.com>
2013-06-27 16:15:16 -0400

    Fixed issue #13 and modified css slighty to fix issue #11

--- DailyEZ.Web/widgets/PageStackSelector/stackSelectorTemplate.js
+++ DailyEZ.Web/widgets/PageStackSelector/stackSelectorTemplate.js
@@ -14,6 +14,11 @@
             url: "widgets/PageStackSelector/PageStackSelectorWorker.ashx",
             success: function (response) {
                 $("#dailyEZ-com-page-stack-selector-widget[%WIDGET-ID%]").html(response);
+                if (string.length("[%TITLE%]") > 12)
+                    $("#dailyEZ-com-page-stack-selector-widther[%WIDGET-ID%] .areaTitle").css("font-size", "16pt");
+                else {
+                    $("#dailyEZ-com-page-stack-selector-widther[%WIDGET-ID%] .areaTitle").css("font-size", "18pt");
+                }
             },
             error: function (ajaxObj, statusText, somethingElse) {
                 $("#dailyEZ-com-page-stack-selector-widget[%WIDGET-ID%]").html("<b>Error retreiving stack selector</b>");
