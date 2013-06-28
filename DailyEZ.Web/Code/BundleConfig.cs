ae799e377f8b4194373f91fbf42561e2f39fe603
Kyle Gobel <kgobel@gmail.com>
2013-06-28 17:34:20 -0400

    made it so on release version grab jqueryUI from google's cdn

--- DailyEZ.Web/Code/BundleConfig.cs
+++ DailyEZ.Web/Code/BundleConfig.cs
@@ -7,21 +7,28 @@ namespace DailyEZ.Web.Code
         public static void RegisterBundles(BundleCollection bundles)
         {
 
-            //BundleTable.EnableOptimizations = true;
-
+            BundleTable.EnableOptimizations = true;
+            bundles.UseCdn = true;
             //jquery
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/js/vendor/jquery-{version}.js"));
 
+            //jquery ui cdn path
+            const string jqueryUiCdn = "http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js";
+            const string jqueryUiCssCdn = "http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/themes/redmond/jquery-ui.min.css";
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui",jqueryUiCdn)
+                .Include(
+                "~/js/vendor/jquery.ui.core.js",
+                "~/js/vendor/jquery.ui.widget.js",
+                "~/js/vendor/jquery.ui.dialog.js",
+                "~/js/vendor/jquery.ui.position.js"
+                )
+                );
             //third party libraries and plugins
             bundles.Add(new ScriptBundle("~/bundles/jsextlibs")
                 .Include(
                 "~/js/vendor/bootstrap.js", //bootstrap
                 //jquery plugins
                 "~/js/vendor/jquery.ba-bbq.js",
-                "~/js/vendor/jquery.ui.core.js",
-                "~/js/vendor/jquery.ui.widget.js",
-                "~/js/vendor/jquery.ui.dialog.js",
-                "~/js/vendor/jquery.ui.position.js",
                 //toastr
                 "~/js/vendor/toastr.js"
                 ));
@@ -35,8 +42,10 @@ namespace DailyEZ.Web.Code
 
 
             //css bundles
-            bundles.Add(new StyleBundle("~/Content/css").IncludeDirectory("~/css/", "*.css", searchSubdirectories: false)
-                .Include("~/css/vendor/jquery.ui.all.css"));
+            bundles.Add(new StyleBundle("~/Content/css").IncludeDirectory("~/css/", "*.css", searchSubdirectories: false));
+
+            bundles.Add(new StyleBundle("~/Content/css/jqueryui",jqueryUiCssCdn).Include("~/css/vendor/jquery.ui.all.css"));
+            
 
 
             //for default aspx
